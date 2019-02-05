2b6965bc8f5c6b9089dd9445c5e93ae2da7c7a4f
Poul Kjeldager Sørensen <pks@s-innovations.net>
2019-02-05 10:25:31 +0100

    fixed odata result

--- src/DotNETDevOps.Identity.AzureB2CUserService/ODataResult.cs
+++ src/DotNETDevOps.Identity.AzureB2CUserService/ODataResult.cs
@@ -1,17 +1,28 @@
-﻿namespace DotNETDevOps.Identity.AzureB2CUserService
+﻿using Newtonsoft.Json;
+
+namespace DotNETDevOps.Identity.AzureB2CUserService
 {
-    /// <summary>
-    /// OData Result
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
+    ///// <summary>
+    ///// OData Result
+    ///// </summary>
+    ///// <typeparam name="T"></typeparam>
+    //public class ODataResult<T>
+    //{
+    //    /// <summary>
+    //    /// The odata result
+    //    /// </summary>
+    //    public T Value { get; set; }
+
+
+    //}
+
     public class ODataResult<T>
     {
-        /// <summary>
-        /// The odata result
-        /// </summary>
+        [JsonProperty("odata.metadata")]
+        public string Metadata { get; set; }
+        [JsonProperty("odata.nextLink")]
+        public string NextLink { get; set; }
         public T Value { get; set; }
-
-
     }
 
 }
